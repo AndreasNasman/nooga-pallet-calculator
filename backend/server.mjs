d5b7ee0d4d10793cd68d733c175cc9d19b2e45d8
Andreas Näsman <andreas.z.nasman@gmail.com>
2020-10-04 11:42:30 +0300

    MongoDB setup

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,9 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const PORT = 4000;
 
+/* MongoDB */
+mongoose.connect("mongodb://localhost/pallets", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("MongoDB connection established successfully.");
+});
+
+/* Express */
 const app = express();
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}.`);
 });
