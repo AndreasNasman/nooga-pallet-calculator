98e2b6c96dc52c97a69a25a9426d6127022d384a
Andreas Näsman <andreas.z.nasman@gmail.com>
2020-10-06 09:36:49 +0300

    Issue 11 connecting the client to the server (#12)
    * Root directory setup
    
    * Creates connection between client and server
    
    * Adds Emmet autocomplete
    
    * Adds Material UI
    
    * Adds delivery information component
    
    * Corrects maximum number of boxes
    
    * Makes properties case match
    
    * Adds server connection
    
    * Adds new pallet button
    
    * Adds icons

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,9 +1,12 @@
 import bodyParser from "body-parser";
+import cors from "cors";
 import express from "express";
 import "./database.mjs";
 import { router } from "./router.mjs";
 
 const app = express();
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/pallets", router);
 
--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@material-ui/core";
+import { Add, Print } from "@material-ui/icons";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { DeliveryInformation } from "./components/DeliveryInformation/DeliveryInformation";
 
-function App() {
+const BASE_URL = "http://localhost:4000";
+
+export const App = () => {
+  const [pallet, setPallet] = useState({});
+
+  const fetchPallet = async () => {
+    const response = await axios(`${BASE_URL}/pallets`);
+
+    if (response.status === 200) {
+      const currentPallet = response.data.pop();
+      if (currentPallet) setPallet(currentPallet);
+    }
+  };
+
+  useEffect(() => {
+    fetchPallet();
+  }, [setPallet]);
+
+  const newPallet = async () => {
+    const response = await axios.post(`${BASE_URL}/pallets/new`, {
+      user: "Andreas Näsman",
+    });
+
+    if (response.status === 200) fetchPallet();
+  };
+
+  const addBox = async (box) => {
+    const response = await axios.post(
+      `${BASE_URL}/pallets/${pallet.id}/add-box`,
+      box
+    );
+
+    if (response.status === 200) fetchPallet();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button
+        color="primary"
+        onClick={newPallet}
+        startIcon={<Add />}
+        variant="contained"
+      >
+        New pallet
+      </Button>
+      <DeliveryInformation addBox={addBox} pallet={pallet} />
+      <Button color="primary" startIcon={<Print />} variant="contained">
+        Print list
+      </Button>
     </div>
   );
-}
-
-export default App;
+};
