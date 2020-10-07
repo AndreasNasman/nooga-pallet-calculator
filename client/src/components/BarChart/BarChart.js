d947aa7ae1b06af51dc58662e5c55a3109b56bd9
Andreas Näsman <andreas.z.nasman@gmail.com>
2020-10-07 10:58:25 +0300

    Issue 8 add pallet box counter (#13)
    * Adds bar chart
    
    * Improves styling
    
    * Adds text to bar chart

---
+++ client/src/components/BarChart/BarChart.js
@@ -0,0 +1,45 @@
+import { Animation, ValueScale } from "@devexpress/dx-react-chart";
+import {
+  BarSeries,
+  Chart,
+  Title,
+  ValueAxis,
+} from "@devexpress/dx-react-chart-material-ui";
+import Paper from "@material-ui/core/Paper";
+import { scaleLinear } from "d3-scale";
+import * as React from "react";
+import styles from "./BarChart.module.css";
+
+const MAX_BOXES = 25;
+
+const scale = scaleLinear();
+scale.ticks = () => [0, 5, 10, 15, 20, 25];
+
+const modifyDomain = () => [0, MAX_BOXES];
+
+export const BarChart = ({ pallet }) => {
+  const numberOfBoxes = pallet.boxes.length;
+  const { id } = pallet;
+
+  const data = [{ id, numberOfBoxes }];
+
+  return (
+    <Paper>
+      <Chart data={data}>
+        <ValueAxis />
+        <ValueScale factory={() => scale} modifyDomain={modifyDomain} />
+
+        <BarSeries valueField="numberOfBoxes" argumentField="id" />
+
+        <Animation />
+        <Title text={id} />
+      </Chart>
+
+      <p className={styles.number_of_boxes}>
+        <strong>Number of boxes on pallet: {numberOfBoxes}</strong>
+        <br />
+        (max {MAX_BOXES})
+      </p>
+    </Paper>
+  );
+};
--- client/src/components/DeliveryInformation/DeliveryInformation.js
+++ client/src/components/DeliveryInformation/DeliveryInformation.js
@@ -1,5 +1,12 @@
-import { Button, Grid, MenuItem, Select, TextField } from "@material-ui/core";
-import { Save } from "@material-ui/icons";
+import {
+  Button,
+  Grid,
+  MenuItem,
+  Paper,
+  Select,
+  TextField,
+} from "@material-ui/core";
+import { Add, Print, Save } from "@material-ui/icons";
 import * as React from "react";
 import { useState } from "react";
 import styles from "./DeliveryInformation.module.css";
@@ -13,7 +20,7 @@ const initialState = {
   recordDate: new Date(),
 };
 
-export const DeliveryInformation = ({ addBox, pallet }) => {
+export const DeliveryInformation = ({ addBox, newPallet, pallet }) => {
   const [
     { amount, articleName, batchNumber, boxNumber, daysToDueDate, recordDate },
     setState,
@@ -51,114 +58,129 @@ export const DeliveryInformation = ({ addBox, pallet }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Grid
-        alignItems="center"
-        className={styles.container}
-        container
-        spacing={3}
+    <Paper className={styles.container}>
+      <Button
+        color="primary"
+        onClick={newPallet}
+        startIcon={<Add />}
+        variant="contained"
       >
-        <Grid item xs={6}>
-          Pallet ID:
-        </Grid>
-        <Grid item xs={6}>
-          <span>{pallet.id}</span>
-        </Grid>
+        New pallet
+      </Button>
 
-        <Grid item xs={6}>
-          Box number:
-        </Grid>
-        <Grid item xs={6}>
-          <TextField
-            name="boxNumber"
-            onChange={handleChange}
-            required
-            type="number"
-            value={boxNumber}
-          />
-        </Grid>
+      <form onSubmit={handleSubmit}>
+        <Grid
+          alignItems="center"
+          className={styles.container}
+          container
+          spacing={3}
+        >
+          <Grid item xs={6}>
+            Pallet ID:
+          </Grid>
+          <Grid item xs={6}>
+            <span>{pallet.id}</span>
+          </Grid>
 
-        <Grid item xs={6}>
-          Article name:
-        </Grid>
-        <Grid item xs={6}>
-          <TextField
-            name="articleName"
-            onChange={handleChange}
-            required
-            value={articleName}
-          />
-        </Grid>
+          <Grid item xs={6}>
+            Box number:
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              name="boxNumber"
+              onChange={handleChange}
+              required
+              type="number"
+              value={boxNumber}
+            />
+          </Grid>
 
-        <Grid item xs={6}>
-          Amount:
-        </Grid>
-        <Grid item xs={6}>
-          <TextField
-            name="amount"
-            onChange={handleChange}
-            required
-            type="number"
-            value={amount}
-          />
-        </Grid>
+          <Grid item xs={6}>
+            Article name:
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              name="articleName"
+              onChange={handleChange}
+              required
+              value={articleName}
+            />
+          </Grid>
 
-        <Grid item xs={6}>
-          Batch number:
-        </Grid>
-        <Grid item xs={6}>
-          <TextField
-            name="batchNumber"
-            onChange={handleChange}
-            required
-            type="number"
-            value={batchNumber}
-          />
-        </Grid>
+          <Grid item xs={6}>
+            Amount:
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              name="amount"
+              onChange={handleChange}
+              required
+              type="number"
+              value={amount}
+            />
+          </Grid>
 
-        <Grid item xs={6}>
-          Record date:
-        </Grid>
-        <Grid item xs={6}>
-          <span>{recordDate.toLocaleDateString("fi-FI")}</span>
-        </Grid>
+          <Grid item xs={6}>
+            Batch number:
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              name="batchNumber"
+              onChange={handleChange}
+              required
+              type="number"
+              value={batchNumber}
+            />
+          </Grid>
 
-        <Grid item xs={6}>
-          Days until last use date:
-        </Grid>
-        <Grid item xs={6}>
-          <Select
-            name="daysToDueDate"
-            onChange={handleChange}
-            required
-            value={daysToDueDate}
-          >
-            {Array.from({ length: 31 }, (_, index) => (
-              <MenuItem key={index} value={index}>
-                {index}
-              </MenuItem>
-            ))}
-          </Select>
-        </Grid>
+          <Grid item xs={6}>
+            Record date:
+          </Grid>
+          <Grid item xs={6}>
+            <span>{recordDate.toLocaleDateString("fi-FI")}</span>
+          </Grid>
 
-        <Grid item xs={6}>
-          Due date:
-        </Grid>
-        <Grid item xs={6}>
-          <span>{dueDate.toLocaleDateString("fi-FI")}</span>
-        </Grid>
+          <Grid item xs={6}>
+            Days until last use date:
+          </Grid>
+          <Grid item xs={6}>
+            <Select
+              name="daysToDueDate"
+              onChange={handleChange}
+              required
+              value={daysToDueDate}
+            >
+              {Array.from({ length: 31 }, (_, index) => (
+                <MenuItem key={index} value={index}>
+                  {index}
+                </MenuItem>
+              ))}
+            </Select>
+          </Grid>
 
-        <Grid item xs={12}>
-          <Button
-            color="primary"
-            startIcon={<Save />}
-            type="submit"
-            variant="contained"
-          >
-            Save
-          </Button>
+          <Grid item xs={6}>
+            Due date:
+          </Grid>
+          <Grid item xs={6}>
+            <span>{dueDate.toLocaleDateString("fi-FI")}</span>
+          </Grid>
+
+          <Grid item xs={12}>
+            <Button
+              color="primary"
+              startIcon={<Save />}
+              type="submit"
+              variant="contained"
+            >
+              Save
+            </Button>
+          </Grid>
         </Grid>
-      </Grid>
-    </form>
+      </form>
+
+      <Button color="primary" startIcon={<Print />} variant="contained">
+        Print list
+      </Button>
+    </Paper>
   );
 };
