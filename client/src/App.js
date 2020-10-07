99943d0bd96ee867db6c5646f722f97cf6a6ccf7
Andreas Näsman <andreas.z.nasman@gmail.com>
2020-10-07 18:21:01 +0300

    Adds backdrop until data is loaded

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { Backdrop, CircularProgress } from "@material-ui/core";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import styles from "./App.module.css";
@@ -8,7 +9,7 @@ const BASE_URL = "http://localhost:4000";
 
 export const App = () => {
   const [pallets, setPallets] = useState([]);
-  const activePallet = pallets[pallets.length - 1] || { boxes: [], id: "" };
+  const activePallet = pallets[pallets.length - 1];
 
   const fetchPallets = async () => {
     const response = await axios(`${BASE_URL}/pallets`);
@@ -36,6 +37,14 @@ export const App = () => {
 
     if (response.status === 200) fetchPallets();
   };
+
+  if (pallets.length === 0) {
+    return (
+      <Backdrop open>
+        <CircularProgress />
+      </Backdrop>
+    );
+  }
 
   return (
     <div className={styles.container}>
